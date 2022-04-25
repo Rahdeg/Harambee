1e49c25127b46f0fb1d15644cb6d1095f7c28086
Rahdeg <walett95@gmail.com>
2022-04-25 18:40:04 +0100

    Updates

---
+++ precache-manifest.bf8255366da5bb9f6019f65115b39af9.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "46a66f91cd475a5adc1a",
+    "url": "/static/css/main.857a3ea0.chunk.css"
+  },
+  {
+    "revision": "46a66f91cd475a5adc1a",
+    "url": "/static/js/main.46a66f91.chunk.js"
+  },
+  {
+    "revision": "eba0a5792f362f0d9c3c",
+    "url": "/static/css/1.6034bcfa.chunk.css"
+  },
+  {
+    "revision": "eba0a5792f362f0d9c3c",
+    "url": "/static/js/1.eba0a579.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0c139b77dc8daa92fe2af2a3999c7c4f",
+    "url": "/static/media/london-stock.0c139b77.jpg"
+  },
+  {
+    "revision": "035f264b989747a6de0fc7fc8a6d4be3",
+    "url": "/static/media/cont.035f264b.jpg"
+  },
+  {
+    "revision": "4c19a3aef3aa1b9fd2d592dc9f0b2ad5",
+    "url": "/static/media/Assorted-Meat.4c19a3ae.png"
+  },
+  {
+    "revision": "1e517b8cc5a526bd87b3d7050f5103ee",
+    "url": "/static/media/Basmati-Rice.1e517b8c.png"
+  },
+  {
+    "revision": "470f1c351b22d0bdb2c6ed4452606419",
+    "url": "/static/media/Bean-Cake.470f1c35.png"
+  },
+  {
+    "revision": "17110de3eb6b276f5d8993b599abd405",
+    "url": "/static/media/Burger.17110de3.png"
+  },
+  {
+    "revision": "3408edd1ddd4cab776d2b8c3ee42c070",
+    "url": "/static/media/Cat-Fish-Pepper-Soup.3408edd1.png"
+  },
+  {
+    "revision": "3f21f82a8c54cb5a9eb8ff05c769dba8",
+    "url": "/static/media/Chinese-Rice-1.3f21f82a.png"
+  },
+  {
+    "revision": "392f323b10ba44b1317c24440af4201a",
+    "url": "/static/media/Eba-Efo-Riro.392f323b.png"
+  },
+  {
+    "revision": "7044497139e3d7fe39b6404050ff84bc",
+    "url": "/static/media/Jollof-Spaghetti.70444971.png"
+  },
+  {
+    "revision": "3d82e36797a81bfcf5080764a28e060a",
+    "url": "/static/media/Meat-Pie.3d82e367.png"
+  },
+  {
+    "revision": "6779c90419809e748914a5326b480a94",
+    "url": "/static/media/Leafy-Rice-Sauce.6779c904.png"
+  },
+  {
+    "revision": "c92bd0fb4db1e89ae775cb070a7140e7",
+    "url": "/static/media/Grillo-Fish.c92bd0fb.png"
+  },
+  {
+    "revision": "fa48292be7f4aa2e20d2cb367f43ef28",
+    "url": "/static/media/Fish-Picata-Salad-1.fa48292b.png"
+  },
+  {
+    "revision": "511ba4965061901a9564d3eddecb5daa",
+    "url": "/static/media/Porridge.511ba496.png"
+  },
+  {
+    "revision": "4bd8b88f45edb86c7d7f5b046a0f76d3",
+    "url": "/static/media/Turkey.4bd8b88f.png"
+  },
+  {
+    "revision": "a318167e19cdc5bed78b0da150723cac",
+    "url": "/static/media/twitter.a318167e.png"
+  },
+  {
+    "revision": "2c1a7560c88ea83e6b2593cd07af8ad8",
+    "url": "/static/media/gmail.2c1a7560.png"
+  },
+  {
+    "revision": "774352deeaa60ebe04164a1e28489771",
+    "url": "/static/media/whatsapp.774352de.png"
+  },
+  {
+    "revision": "5c570427ee23f69853d28aec805eee79",
+    "url": "/static/media/instagram.5c570427.png"
+  },
+  {
+    "revision": "1e47e35bb7b2d5c5961a89fd6e934bcb",
+    "url": "/index.html"
+  }
+];
