b75c1c5d049a329bc3e43131cc2fed55ef2773fa
Lordson Silver <lordsonochuko5@gmail.com>
2022-04-27 12:50:37 +0100

    Update home.js
    Import image

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from './Button';
 import './Home.css';
+import Hotel from './HotelPic.jpg';
 
 const home = () => {
   return (
     <div className='Home-container'>
-       <img src="/HotelPic.jpg" alt='hotelpic' className='foto'/>
+       <img src={Hotel} alt='hotelpic' className='foto' />
       
        <h1 >WELCOME TO HARAMBEE</h1>
        <p>Where your problems fade away</p>
